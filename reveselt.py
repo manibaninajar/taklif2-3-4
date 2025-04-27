54f798624c1cbea35570b33c83828b49e93d1fca
manibaninajar <manibaninajar33@gmail.com>
2025-04-27 14:51:44 +0330

    reveselt.py

--- reveselt.py
+++ reveselt.py
@@ -1,15 +1,12 @@
-# 1. Get input from the user
-text = input("Please enter a string: ")
+# Get text from the user
+text = input("Enter a text: ")
 
-# 2. Create an empty string to store the reversed text
+# Create an empty string to store the reversed text
 reversed_text = ""
 
-# 3. Start from the last character and go backwards
-i = len(text) - 1  # Index of the last character
+# Use a for loop to go from the end of the text to the beginning
+for i in range(len(text) - 1, -1, -1):
+    reversed_text = reversed_text + text[i]
 
-while i >= 0:
-    reversed_text = reversed_text + text[i]  # Add the character to the result
-    i = i - 1  # Move one step back
-
-# 4. Print the reversed string
-print("Reversed string:", reversed_text)
+# Print the reversed text
+print(reversed_text)
